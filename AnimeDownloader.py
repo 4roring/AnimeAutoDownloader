37bc3de0a62f341cf988f005b935f355ad6647ac
4roring <4roring@naver.com>
2021-07-29 22:19:13 +0900

    Yaml 파일 사용하여 다운로드 받을 수 있도록 개선

--- AnimeDownloader.py
+++ AnimeDownloader.py
@@ -1,6 +1,7 @@
 import os
 import feedparser
 import time
+import yaml
 from requests import get as http_get
 from qbittorrent import Client
 from plexapi.server import PlexServer
@@ -12,12 +13,15 @@ def plex_sync():
     plex = PlexServer(plex_url, token)
     movie = plex.library.section('영화')
     movie.update()
+    print('Plex Server Sync Complete')
 
 
 def download(download_url: str, download_path: str):
     torrent_url = 'http://127.0.0.1:8080/'
     torrent_id = '4roring'
     torrent_pwd = ''
+
+    # QBTorrent
 
     qb = Client(torrent_url)
     qb.login(torrent_id, torrent_pwd)
@@ -30,9 +34,15 @@ def download(download_url: str, download_path: str):
     while True:
         qb_main_data = qb.sync_main_data()
 
+        time.sleep(1)
+
         torrents: dict = qb_main_data['torrents']
+        if not torrents:
+            break
+
         for hash_key, torrent in torrents.items():
-            if not 'downloading' == torrent['state']:
+            current_state = torrent['state']
+            if 'completed' == current_state or 'stalledUP' == current_state:
                 qb.delete([hash_key])
                 break
 
@@ -42,15 +52,21 @@ def download(download_url: str, download_path: str):
 
 
 if __name__ == '__main__':
-    keyword = 'digimon'
-    url = 'https://nyaa.si/?page=rss&q={}&c=0_0&f=0'.format(keyword)
+    with open('getter_arc.yaml', 'r') as file:
+        down_info = yaml.load(file, Loader=yaml.FullLoader)
+        print(down_info)
 
-    episode_num = '49'
-    download_name = '[Erai-raws] SSSS.Dynazenon - {}'.format(episode_num)
-    # download_name = '[Erai-raws] Digimon Adventure (2020) - {}'.format(episode_num)
+    keyword = down_info['keyword']
     quality = ''
+    episode_num = str(down_info['episode']).zfill(2)
+    download_name = down_info['download_name'].format(episode_num)
+    download_path = down_info['download_path']
 
-    download_path = 'D:/Movie/SSSS.Dynazenon'
+    url = f'https://nyaa.si/?page=rss&q={keyword}&c=0_0&f=0'
+
+    # keyword = 'tensei+slime'
+    # download_name = '[SSA] Tensei shitara Slime Datta Ken - {} [1080p]'.format(episode_num)
+    # download_name = '[Erai-raws] Digimon Adventure (2020) - {}'.format(episode_num)
 
     if not os.path.exists(download_path):
         os.mkdir(download_path)
